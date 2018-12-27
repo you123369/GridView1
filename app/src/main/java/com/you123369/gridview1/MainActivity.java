924147f0dcfb532373814edad9e2b95caaa843fa
you123369 <you123369999999@gmail.com>
2018-12-27 12:08:50 +0700

    Initial commit

---
+++ app/src/main/java/com/you123369/gridview1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.you123369.gridview1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(MainActivity.this,""+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
